f3aa72033b5a64588e593c8d965543d19c236b5a
Kacper Majcher <kacpermajcher13@gmail.com>
2024-07-04 14:54:14 +0200

    Create AuthRepository

---
+++ lib/features/auth/data/domain/repositories/auth_repository.dart
@@ -0,0 +1,72 @@
+import 'package:two_ticket/features/auth/data/datasources/auth_local_data_source.dart';
+import 'package:two_ticket/features/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:two_ticket/features/auth/data/domain/model/login_model.dart';
+import 'package:two_ticket/features/auth/data/domain/model/user_model.dart';
+
+class AuthRepository {
+  final AuthRemoteDataSource remoteDataSource;
+  final AuthLocalDataSource localDataSource;
+
+  AuthRepository({
+    required this.remoteDataSource,
+    required this.localDataSource,
+  });
+
+  Future<User> login(
+    String username,
+    String password,
+  ) async {
+    final loginModel = LoginModel(
+      username: username,
+      password: password,
+    );
+
+    try {
+      final response = await remoteDataSource.login(
+        loginModel.toJson(),
+      );
+
+      if (response.response.statusCode == 200 ||
+          response.response.statusCode == 204) {
+        List<String>? cookies = response.response.headers['set-cookie'];
+        String? cookie = cookies?.first;
+
+        if (cookie != null) {
+          await localDataSource.saveCookie(cookie);
+          await localDataSource.saveUsername(username);
+
+          return User(
+            username: username,
+            cookie: cookie,
+          );
+        } else {
+          throw Exception('Login successful but cookie not found in response');
+        }
+      } else {
+        throw Exception(
+            'Login failed. Status code: ${response.response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Request failed: $e');
+    }
+  }
+
+  Future<void> logout() async {
+    await localDataSource.clearCookie();
+    await localDataSource.clearUsername();
+  }
+
+  Future<User?> getCachedUser() async {
+    final cookie = await localDataSource.getCookie();
+    final username = await localDataSource.getUsername();
+
+    if (cookie != null && username != null) {
+      return User(
+        username: username,
+        cookie: cookie,
+      );
+    }
+
+    return null;
+  }
+}
