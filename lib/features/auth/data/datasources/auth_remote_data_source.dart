26259dd5a869e7872faf28e5ef94544eae61cdb8
Kacper Majcher <kacpermajcher13@gmail.com>
2024-07-04 14:52:33 +0200

    Create AuthRemoteDataSource

---
+++ lib/features/auth/data/datasources/auth_remote_data_source.dart
@@ -0,0 +1,21 @@
+import 'package:dio/dio.dart';
+import 'package:injectable/injectable.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:two_ticket/core/constants/constants.dart';
+
+part 'auth_remote_data_source.g.dart';
+
+@RestApi(baseUrl: apiBaseURL)
+@injectable
+abstract class AuthRemoteDataSource {
+  @factoryMethod
+  factory AuthRemoteDataSource(
+    Dio dio, {
+    String baseUrl,
+  }) = _AuthRemoteDataSource;
+
+  @POST(endpointLogin)
+  Future<HttpResponse> login(
+    @Body() Map<String, dynamic> body,
+  );
+}
